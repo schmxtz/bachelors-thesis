c9ff54821747789b64514c738032285a0cd74061
Philipp Schmitz <schmitph@hochschule-trier.de>
2023-02-17 14:55:18 +0100

    Changed logical order of if-cases to improve performance

--- pdf/PDFGenerator.py
+++ pdf/PDFGenerator.py
@@ -111,14 +111,13 @@ class PDFGenerator:
         """
         line = paragraph.runs
         for word in line:
-            if word.text in parameter:
-                value = parameter.get(word.text)
-                if value is None:
-                    value = ''
-                word.text = word.text.replace(word.text, value)
-            else:
-                # Check if the current word is a placeholder
-                if PLACEHOLDER_OPENING in word.text and PLACEHOLDER_CLOSING in word.text:
+            if PLACEHOLDER_OPENING in word.text and PLACEHOLDER_CLOSING in word.text:
+                if word.text in parameter:
+                    value = parameter.get(word.text)
+                    if value is None:
+                        value = ''
+                    word.text = word.text.replace(word.text, value)
+                else:
                     word.text = word.text.replace(word.text, '')
 
     @staticmethod
